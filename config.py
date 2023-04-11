02b6637cf3c35652eb35acf03f62a1e30d73bcf1
xtineyowww <130447265+xtineyowww@users.noreply.github.com>
2023-04-11 16:42:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cpaqpifxmlhl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "christineyowsuetyi-employee"
+customregion = "us-east-1"
